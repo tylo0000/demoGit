15d4cb0d89b8508b636acab4187c333f021ff1e4
vo iu <vietrogalaxy@gmail.com>
2017-05-06 14:32:42 +0700

    the dog jump hight

--- demoGit/ViewController.m
+++ demoGit/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"hello World");
+    NSLog(@"The DOG jump hight");
 }
 
 
