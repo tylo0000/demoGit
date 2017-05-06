0da8a099efbdcc16bf9cc01ac4073c760fde5b56
vo iu <vietrogalaxy@gmail.com>
2017-05-06 13:59:47 +0700

    Initial Commit

---
+++ demoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoGit
+//
+//  Created by Shiube on 5/6/17.
+//  Copyright © 2017 ThangNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoGit
+//
+//  Created by Shiube on 5/6/17.
+//  Copyright © 2017 ThangNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
